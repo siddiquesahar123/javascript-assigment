b99a3ac3225dcb9f5a66d50be42472c1042e67e1
siddiquesahar123 <126084762+siddiquesahar123@users.noreply.github.com>
2023-04-04 08:15:01 -0700

    Add files via upload

---
+++ javascript/assests/js/app.js
@@ -0,0 +1,15 @@
+// Q:1  Write a script to greet your website visitor using JS alert box?
+alert("Hello World!");
+
+// Q:2  Write a script to display following message on your web page?
+alert("Error! Please enter a valid password");
+
+// Q:3 Write a script to display following message on your web page?
+alert("Welcome to JS land...\nHappy coding");
+
+// Q:4  Write a script to display following messages in sequence pages
+alert("Welcome to JS Land...");
+alert("Happy Coding\nPrevent this page creatng addional dialogue");
+
+// Q:5  Generate the following message through browser’s developer console?
+alert("Hello...I can run JS through my web browsers console");
